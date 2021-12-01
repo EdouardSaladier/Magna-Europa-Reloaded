fdcc105c3b1a42eb513fd2f0a6d9c23fba09b7f3
Jeansaisrien <alexandre.evain76@gmail.com>
2021-12-01 21:38:08 +0100

    England

---
+++ common/defines/EDO_defines.lua
@@ -0,0 +1,15 @@
+-- NDefines.NGame.START_DATE = "2033.1.1.12"
+-- NDefines.NGame.END_DATE = "2070.1.1.1"
+-- NDefines.NGame.MAP_SCALE_PIXEL_TO_KM = 4
+-- NDefines.NGame.SAVE_VERSION = 7
+
+-- NDefines.NMilitary.LAND_SPEED_MODIFIER = 0.15
+
+-- NDefines.NAir.AIR_REGION_SUPERIORITY_PIXEL_SCALE = 0.02
+
+-- NDefines.NNavy.NAVAL_SPEED_MODIFIER = 0.3
+-- NDefines.NNavy.NAVAL_TRANSFER_BASE_SPEED = 6
+
+NDefines.NFrontend.CAMERA_MIN_HEIGHT = 50.0
+NDefines.NFrontend.CAMERA_MAX_HEIGHT = 3800.0
+NDefines.NGraphics.PROVINCE_NAME_DRAW_DISTANCE = 1000.0 			-- Remove province names beyond this distance
