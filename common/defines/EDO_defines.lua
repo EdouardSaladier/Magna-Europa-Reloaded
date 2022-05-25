21e2ae3788a51ac55ab6ddfa6a96cdec8a720ed6
EdouardSaladier <57146917+EdouardSaladier@users.noreply.github.com>
2022-05-25 19:21:30 +0200

    It is now possible to launch and play the game

--- common/defines/EDO_defines.lua
+++ common/defines/EDO_defines.lua
@@ -1,14 +1,5 @@
--- NDefines.NGame.START_DATE = "2033.1.1.12"
--- NDefines.NGame.END_DATE = "2070.1.1.1"
--- NDefines.NGame.MAP_SCALE_PIXEL_TO_KM = 4
--- NDefines.NGame.SAVE_VERSION = 7
-
--- NDefines.NMilitary.LAND_SPEED_MODIFIER = 0.15
-
--- NDefines.NAir.AIR_REGION_SUPERIORITY_PIXEL_SCALE = 0.02
-
--- NDefines.NNavy.NAVAL_SPEED_MODIFIER = 0.3
--- NDefines.NNavy.NAVAL_TRANSFER_BASE_SPEED = 6
+NDefines.NGame.START_DATE = "1936.1.1.12"
+NDefines.NGame.END_DATE = "1970.1.1.1"
 
 NDefines.NFrontend.CAMERA_MIN_HEIGHT = 50.0
 NDefines.NFrontend.CAMERA_MAX_HEIGHT = 3800.0
