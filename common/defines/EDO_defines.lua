dd1374d633cc001e4995471dbc3b5286f5c27ac4
EdouardSaladier <57146917+EdouardSaladier@users.noreply.github.com>
2022-05-25 23:20:36 +0200

    Fixed Lakes borders

--- common/defines/EDO_defines.lua
+++ common/defines/EDO_defines.lua
@@ -4,3 +4,14 @@ NDefines.NGame.END_DATE = "1970.1.1.1"
 NDefines.NFrontend.CAMERA_MIN_HEIGHT = 50.0
 NDefines.NFrontend.CAMERA_MAX_HEIGHT = 3800.0
 NDefines.NGraphics.PROVINCE_NAME_DRAW_DISTANCE = 1000.0 			-- Remove province names beyond this distance
+
+NDefines.NGame.MAP_SCALE_PIXEL_TO_KM = 4
+NDefines.NGame.SAVE_VERSION = 7
+
+NDefines.NMilitary.LAND_SPEED_MODIFIER = 0.15
+
+NDefines.NAir.AIR_REGION_SUPERIORITY_PIXEL_SCALE = 0.02
+
+--NDefines.NNavy.NAVAL_SPEED_MODIFIER = 0.3
+--NDefines.NNavy.NAVAL_TRANSFER_BASE_SPEED = 6
+NDefines.NNavy.NAVAL_RANGE_TO_INGAME_DISTANCE = 0.24							-- Scale the ship stats "naval_range" to the ingame distance
