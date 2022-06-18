0c657a88ea9bf90ddfd622f19ecebde63014c673
EdouardSaladier <57146917+EdouardSaladier@users.noreply.github.com>
2022-06-18 20:21:39 +0200

    Adjusted supplies and building

--- common/defines/EDO_defines.lua
+++ common/defines/EDO_defines.lua
@@ -10,18 +10,19 @@ NDefines.NGame.SAVE_VERSION = 7
 
 NDefines.NMilitary.LAND_SPEED_MODIFIER = 0.15
 
-NDefines.NSupply.CAPITAL_SUPPLY_BASE = 1.2 -- base supply for capital
-NDefines.NSupply.CAPITAL_INITIAL_SUPPLY_FLOW = 1.2 -- starting supply from capital
-NDefines.NSupply.CAPITAL_STARTING_PENALTY_PER_PROVINCE = 0.2 -- starting penalty that will be added as supply moves away from its origin (modified by stuff like terrain)
-NDefines.NSupply.CAPITAL_ADDED_PENALTY_PER_PROVINCE = 0.6 -- added penalty as we move away from origin
-NDefines.NSupply.NODE_INITIAL_SUPPLY_FLOW = 0.8
-NDefines.NSupply.NODE_STARTING_PENALTY_PER_PROVINCE = 0.2
-NDefines.NSupply.NODE_ADDED_PENALTY_PER_PROVINCE = 0.2
+NDefines.NSupply.CAPITAL_SUPPLY_BASE = 1.6 -- base supply for capital
+NDefines.NSupply.CAPITAL_INITIAL_SUPPLY_FLOW = 3.0 -- starting supply from capital
+NDefines.NSupply.CAPITAL_STARTING_PENALTY_PER_PROVINCE = 0.3 -- starting penalty that will be added as supply moves away from its origin (modified by stuff like terrain)
+NDefines.NSupply.CAPITAL_ADDED_PENALTY_PER_PROVINCE = 0.4 -- added penalty as we move away from origin
+NDefines.NSupply.NODE_INITIAL_SUPPLY_FLOW = 2.0
+NDefines.NSupply.NODE_STARTING_PENALTY_PER_PROVINCE = 0.3
+NDefines.NSupply.NODE_ADDED_PENALTY_PER_PROVINCE = 0.4
 NDefines.NSupply.INFRA_TO_SUPPLY = 0.15							-- each level of infra gives this many supply
-NDefines.NSupply.VP_TO_SUPPLY_BONUS_CONVERSION = 0.01			-- Bonus to supply local supplies from Victory Points, multiplied by this aspect and rounded to closest integer
+NDefines.NSupply.VP_TO_SUPPLY_BONUS_CONVERSION = 0.02			-- Bonus to supply local supplies from Victory Points, multiplied by this aspect and rounded to closest integer
+NDefines.NSupply.VP_TO_SUPPLY_BASE = 0							-- Bonus to supply from a VP, no matter the level
 NDefines.NSupply.NAVAL_BASE_INITIAL_SUPPLY_FLOW = 0.8
-NDefines.NSupply.NAVAL_BASE_STARTING_PENALTY_PER_PROVINCE = 0.3
-NDefines.NSupply.NAVAL_BASE_ADDED_PENALTY_PER_PROVINCE = 0.4
+NDefines.NSupply.NAVAL_BASE_STARTING_PENALTY_PER_PROVINCE = 0.6
+NDefines.NSupply.NAVAL_BASE_ADDED_PENALTY_PER_PROVINCE = 0.6
 NDefines.NSupply.FLOATING_HARBOR_INITIAL_SUPPLY_FLOW = 0.8
 
 NDefines.NAir.AIR_REGION_SUPERIORITY_PIXEL_SCALE = 0.02
